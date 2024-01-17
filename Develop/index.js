1da4d2d36e57dafbf72b4a7bda9d450c76c56356
C A <chadandari@outlook.com>
2024-01-17 18:06:00 -0500

    Comments added

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,7 @@ const  inquirer = require("inquirer"); //This calls the inquirer package
 const fs = require('fs'); //This calls the filesystem package
 const {Circle, Square, Triangle} = require("./lib/shapes"); //This lets us use our shape classes
 
-
+//This prompts the user and delivers an object containing their responses.
 async function promptUser(){
     return inquirer.prompt([
         {
@@ -38,16 +38,20 @@ async function promptUser(){
     ])
 }
 
+//This builds the SVG string for us.
 function buildSvg(shape, textColor, text){
     const txt = `<text x="50%" y="65%" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>`;
     let str = `<svg version='1.1' xmlns="http://www.w3.org/2000/svg" height='200px' width='300px'>${shape}${txt}</svg>`;
     return str;
 }
 
+//Async function that waits for the user to enter all of the data.
 async function init() {
     const data = await promptUser().then((data => {
         console.log(data);
         let text = "";
+
+        //Check that the length of the logo letters is 3, and only use 3 if it's longer.
         if (data.letters.length > 3){
             console.log("You entered more than three letters, only the first three will be used!");
             text = data.letters.slice(0,3);
@@ -59,6 +63,7 @@ async function init() {
         let bgColor = data.backgroundColor;
         let selectedShape = data.shape;
         let svgStr = "";
+        
         if (selectedShape === "triangle"){
             const tri = new Triangle();
             tri.setColor(bgColor);   
@@ -73,16 +78,19 @@ async function init() {
             svgStr = buildSvg(squ.render(), color, text);
         }
 
+        //Write the string to a file.
         writeToFile("logo.svg", svgStr);
     }));
 }
 
+//write file fn
 function writeToFile(fileName, str) {
     fs.writeFile(fileName, str, (err) =>
     err ? console.log(err) : console.log("Generated logo.svg"));
 }
 
 
+//initialize program.
 init();
 
 
