5ab58bd15b0dab78bc534c71148aef0e4503c517
C A <chadandari@outlook.com>
2024-01-17 18:00:42 -0500

    tests working and passed

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,8 @@
-//Global variables
+//Global variables/packages
 const  inquirer = require("inquirer"); //This calls the inquirer package
 const fs = require('fs'); //This calls the filesystem package
+const {Circle, Square, Triangle} = require("./lib/shapes"); //This lets us use our shape classes
+
 
 async function promptUser(){
     return inquirer.prompt([
@@ -11,13 +13,13 @@ async function promptUser(){
         },
         {
             type:"input",
-            name:"text",
-            message:"Enter a text color keyword or hexidecimal number: ",
+            name:"textColor",
+            message:"Enter a color keyword or hexidecimal number for your text: ",
         },
         {
             type:"input",
-            name:"background",
-            message:"Enter a background color keyword or hexidecimal number: ",
+            name:"backgroundColor",
+            message:"Enter a color keyword or hexidecimal number for your shape's background: ",
         },
         {//This is a list, rather than an input so that we can provide a badge for the user's selection. 
             type:"list",
@@ -36,12 +38,51 @@ async function promptUser(){
     ])
 }
 
+function buildSvg(shape, textColor, text){
+    const txt = `<text x="50%" y="65%" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>`;
+    let str = `<svg version='1.1' xmlns="http://www.w3.org/2000/svg" height='200px' width='300px'>${shape}${txt}</svg>`;
+    return str;
+}
+
 async function init() {
     const data = await promptUser().then((data => {
-        // const icon = sortIcon(data.license);
-        // const markdown = generateMarkdown(data,icon);
-        // writeToFile("README.md", markdown);
+        console.log(data);
+        let text = "";
+        if (data.letters.length > 3){
+            console.log("You entered more than three letters, only the first three will be used!");
+            text = data.letters.slice(0,3);
+        } else if (data.letters.length <= 3){
+            text = data.letters;
+        }
+
+        let color = data.textColor;
+        let bgColor = data.backgroundColor;
+        let selectedShape = data.shape;
+        let svgStr = "";
+        if (selectedShape === "triangle"){
+            const tri = new Triangle();
+            tri.setColor(bgColor);   
+            svgStr = buildSvg(tri.render(), color, text);
+        } else if (selectedShape === "circle"){
+            const cir = new Circle();
+            cir.setColor(bgColor);   
+            svgStr = buildSvg(cir.render(), color, text);
+        } else if (selectedShape === "square"){
+            const squ = new Square();
+            squ.setColor(bgColor);   
+            svgStr = buildSvg(squ.render(), color, text);
+        }
+
+        writeToFile("logo.svg", svgStr);
     }));
 }
 
+function writeToFile(fileName, str) {
+    fs.writeFile(fileName, str, (err) =>
+    err ? console.log(err) : console.log("Generated logo.svg"));
+}
+
+
 init();
+
+
---
+++ Develop/tests/shapes.test.js
@@ -0,0 +1,28 @@
+const {Circle, Triangle, Square} = require("../lib/shapes");
+
+describe("Circle", () => {
+    it("Should return the circle string", () => {
+        const shape = new Circle();
+        let color = "orange";
+        shape.setColor(color);
+        expect(shape.render()).toEqual(`<circle cx="50%" cy="50%" r="100" height="100%" width="100%" fill="${color}">`);
+        });
+});
+
+describe("Triangle", () => {
+    it("Should return the triangle string", () => {
+        const shape = new Triangle();
+        let color = "orange";
+        shape.setColor(color);
+        expect(shape.render()).toEqual(`<polygon height="100%" width="100%" points="0,200 300,200 150,0" fill="${color}">`);
+        });
+});
+
+describe("Square", () => {
+    it("Should return the square string", () => {
+        const shape = new Square();
+        let color = "orange";
+        shape.setColor(color);
+        expect(shape.render()).toEqual(`<rect x="50" height="200" width="200" fill="${color}">`);
+        });
+});
